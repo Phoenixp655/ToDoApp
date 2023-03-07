38d0a985b82643c4e75ef0bbc39741fcc9a1cdb4
hoanganhle <phoenixp655@gmail.com>
2023-03-07 13:28:34 +0700

    config tailwindcss

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/src/views');
 
+app.use('/public', express.static(__dirname + '/src/public'))
+
 
 app.get('/', (req, res) => res.render('index'))
 
