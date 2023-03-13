456d48447538a101142b83eba7ebe8cb9d7c7b05
hoanganhle <phoenixp655@gmail.com>
2023-03-13 15:41:22 +0700

    update home page  header and main UI

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 require('dotenv').config();
+const compression = require('compression');
 const connectDB = require('./src/config/DBConfig');
 const app = express();
 
 connectDB();
+
+app.use(compression());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,6 +17,15 @@ app.set('views', __dirname + '/src/views');
 app.use('/public', express.static(__dirname + '/src/public'));
 
 app.use('/api/todo', require('./src/routers/toDoRouter'));
+
+//response caching header middleware for get request
+app.use((req, res, next) => {
+    const period = 60 * 5;
+    req.method == 'GET'
+        ? res.set('Cache-control', `public, max-age=${period}`)
+        : res.set('Cache-control', 'no-store');
+    next();
+});
 
 app.get('/', (req, res) => res.render('index'));
 
